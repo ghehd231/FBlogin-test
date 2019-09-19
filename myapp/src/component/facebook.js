807c7c481ee77024ff9e6db010200106f655ef62
ghehd231 <ghehd231@naver.com>
2019-09-19 22:00:15 +0900

    react-facebook-login install

--- myapp/src/component/facebook.js
+++ myapp/src/component/facebook.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
+import FacebookLogin from 'react-facebook-login';
 
 class facebook extends Component {
+
+    responseFacebook(response) {
+        console.log(response);
+    }
+   
     render() {
         return (
-            <div>
-                test
-            </div>
-        );
+            <FacebookLogin
+            appId="473667476518556"
+            autoLoad={true}
+            fields="name,email,picture"
+            scope="public_profile,user_friends,user_actions.books"
+            callback={this.responseFacebook}
+            />
+        )
     }
 }
 
